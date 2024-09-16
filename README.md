825bcca9a9d10038ec5a55d691dce48589953e04
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-16 16:33:46 +0530

    feat: complete recursive traversal

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# recursion
+# Recursion
+
+## Links
+
+### `recursiveTraversal.js`
+
+- <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values>
+- <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray>
+- <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce>
---
+++ recursiveTraversal.js
@@ -0,0 +1,43 @@
+// =================================
+// RECURSIVE TRAVERSAL
+// =================================
+
+// Traverse the following object and get the sum of salaries
+let company = {
+    sales: [{
+      name: 'John',
+      salary: 1000
+    }, {
+      name: 'Alice',
+      salary: 1600
+    }],
+  
+    development: {
+      sites: [{
+        name: 'Peter',
+        salary: 2000
+      }, {
+        name: 'Alex',
+        salary: 1800
+      }],
+  
+      internals: [{
+        name: 'Jack',
+        salary: 1300
+      }]
+    }
+  };
+
+function sumSalaries (department) {
+    if (Array.isArray(department)) {
+        return department.reduce((prev, current) => prev + current.salary, 0);
+    } else {
+        let sum = 0;
+        for (let subDepartment of Object.values(department)) {
+            sum += sumSalaries(subDepartment);
+        }
+        return sum;
+    }
+}
+
+console.log(sumSalaries(company));
